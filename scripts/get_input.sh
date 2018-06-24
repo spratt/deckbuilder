41bcd58aaf8659a258e7ff714a2d79f5015ffbfe
Simon Pratt <simon@pr4tt.com>
2018-06-24 12:54:17 -0400

    Reorganize for deploy

---
+++ scripts/get_input.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -X "GET" -H "Accept:\ application/json" -H "Content-type:\ application/x-www-form-urlencoded" https://netrunnerdb.com/api/2.0/public/cards > ../data/cards_input.json
+curl -X "GET" -H "Accept:\ application/json" -H "Content-type:\ application/x-www-form-urlencoded" https://netrunnerdb.com/api/2.0/public/packs > ../data/packs_input.json
