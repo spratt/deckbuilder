a1e0dc1ccf3e691e417548418b9e80abd7b4b302
Simon Pratt <simon@pr4tt.com>
2018-06-24 06:27:24 -0400

    get_input now gets packs

--- get_input.sh
+++ get_input.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl -X "GET" -H "Accept:\ application/json" -H "Content-type:\ application/x-www-form-urlencoded" https://netrunnerdb.com/api/2.0/public/cards > cards_input.json
+curl -X "GET" -H "Accept:\ application/json" -H "Content-type:\ application/x-www-form-urlencoded" https://netrunnerdb.com/api/2.0/public/packs > packs_input.json
